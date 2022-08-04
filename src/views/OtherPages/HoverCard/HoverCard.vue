95d3a1d80a5c2c6deb4e49da6952102bf1a5bb59
Seaming <87215099+SMingC@users.noreply.github.com>
2022-08-04 14:55:50 +0800

    three.js 试用

--- src/views/OtherPages/HoverCard/HoverCard.vue
+++ src/views/OtherPages/HoverCard/HoverCard.vue
@@ -15,7 +15,33 @@ export default {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: "red" });
     const mesh = new THREE.Mesh(geometry, material);
+
+    // Position
+    // mesh.position.x = 0.7;
+    // mesh.position.y = -0.6;
+    // mesh.position.z = 1;
+
+    // mesh.position.set(0.7, -0.6, 1);
     scene.add(mesh);
+
+    // Scale
+    // 也能用 set 方法 mesh.scale.set( <number>,<number>,<number>);
+    // mesh.scale.y = 0.5;
+    // mesh.scale.x = 2;
+    // mesh.scale.z = 0.5;
+
+    // Rotation
+    mesh.rotation.reorder("YXZ");
+    // mesh.rotation.y = Math.PI * 0.25;
+    // mesh.rotation.x = Math.PI * 0.25;
+
+    // Objects
+    const group = new THREE.Group();
+    scene.add(group);
+
+    // Axes helper
+    // const axesHelper = new THREE.AxesHelper();
+    // scene.add(axesHelper);
 
     // Sizes
     const sizes = { width: 800, height: 600 };
@@ -25,14 +51,34 @@ export default {
     camera.position.z = 3;
     scene.add(camera);
 
+    camera.lookAt(mesh.position);
+
     // Render
-    console.log(container);
+    // console.log(container);
     const renderer = new THREE.WebGLRenderer({
       canvas: container,
     });
     renderer.setSize(sizes.width, sizes.height);
 
-    renderer.render(scene, camera);
+    // Clock
+    const clock = new THREE.Clock();
+
+    // Animations
+    const tick = () => {
+      // clock
+      const elapsedTime = clock.getElapsedTime();
+
+      // Update objects
+      mesh.position.y = Math.sin(elapsedTime);
+      mesh.position.x = Math.cos(elapsedTime);
+
+      // Render
+      renderer.render(scene, camera);
+
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
   },
 };
 </script>
